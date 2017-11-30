a5c2a7ed463bf7df4696128a3bd19e84f0cf0700
ahmet fidan <a_fidan135@hotmail.com>
2017-11-30 17:57:31 +0300

    adminpage

---
+++ PT.Entitiy/ViewModel/UsersViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PT.Entitiy.ViewModel
+{
+    public class UsersViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
---
+++ PT.Web.MVC/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using PT.BLL.AccountRepository;
+using PT.Entitiy.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PT.Web.MVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var userManager = MembershipTools.NewUserManager();
+            var users = userManager.Users.Select(x => new UsersViewModel
+            {
+                Email = x.Email,
+                Name = x.Name,
+                RegisterDate = x.RegisterDate,
+                Salary = x.Salary,
+                Surname = x.Surname,
+                UserId = x.Id,
+                UserName = x.UserName,
+                RoleId = x.Roles.FirstOrDefault().RoleId,
+                RoleName = MembershipTools.NewRoleManager().FindById(x.Roles.FirstOrDefault().RoleId).Name
+            }).ToList();
+            var roles = MembershipTools.NewRoleManager().Roles.ToList();
+            List<SelectListItem> rolList = new List<SelectListItem>();
+            roles.ForEach(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id
+            });
+            ViewBag.roles = rolList;
+            return View(users);
+        }
+    }
+}
